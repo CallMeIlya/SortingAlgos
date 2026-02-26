074808e0680336130eefea58e2874e8393424920
crystal <lee.kuznetsov@gmail.com>
2026-02-26 19:32:56 +0000

    just added some stuff

--- sortingalgos.cpp
+++ sortingalgos.cpp
@@ -5,18 +5,24 @@
 
 using namespace std;
 
-template<typename  T>
-int getArrLen(T arr[]) {
+template<typename  Type>
+int getArrLen(Type arr[]) {
   if(sizeof(arr) <= 0) {return 0;}
-  return sizeof(arr)/sizeof(T);
+  return sizeof(arr)/sizeof(Type);
+}
+
+template<typename Type>
+void outputArr(Type arr[]) {
+  int arrLen = getArrLen(arr);
+  for(int i = 0; i < arrLen; i++) {
+    cout << arr[i] << " ";
+  }
+  cout << endl;
 }
 
 int main() {
   int arr[] = {2,5,3,6,18,5,99};
-  int arrLen = getArrLen(arr);
-  for(auto i = 0; i < arrLen; i++) {
-    cout << arr[i] << " ";
-  }
+  outputArr(arr);
   //do some sorting
-
+  outputArr(arr);
 }
